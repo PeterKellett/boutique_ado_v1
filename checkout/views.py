db268f849fc585a8ceaa51395f43f67b0e67e47d
PeterKellett <peterwkellett@gmail.com>
2020-10-14 11:28:28 +0000

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Hc6RJCM38ctaxuamPwSiRWXRaZdcUO0lkMBtah39LWPAWeZ5miVTI5AFZ6HKoKxramtCHnNbWN65KpnsG8zwhHq00PNDuqE0S',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
