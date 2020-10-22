3d97a6353cc959ed1c4d7eb8b0fcb26386dc3de9
PeterKellett <peterwkellett@gmail.com>
2020-10-22 16:33:25 +0000

    Added profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,5 @@
+# For uuid docs
+# See https://docs.djangoproject.com/en/3.1/ref/models/fields/#uuidfield
 import uuid
 
 from django.db import models
@@ -7,6 +9,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 # Create your models here.
@@ -14,6 +17,11 @@ class Order(models.Model):
     order_number = models.CharField(max_length=32,
                                     null=False,
                                     editable=False)
+    user_profile = models.ForeignKey(UserProfile,
+                                     on_delete=models.SET_NULL,
+                                     null=True,
+                                     blank=True,
+                                     related_name='order')
     full_name = models.CharField(max_length=50,
                                  null=False,
                                  blank=False)
@@ -27,7 +35,6 @@ class Order(models.Model):
                            null=False,
                            blank=False)
     postcode = models.CharField(max_length=20,
-                                null=True,
                                 blank=True)
     town_or_city = models.CharField(max_length=40,
                                     null=False,
@@ -36,10 +43,8 @@ class Order(models.Model):
                                        null=False,
                                        blank=False)
     street_address2 = models.CharField(max_length=80,
-                                       null=True,
                                        blank=True)
     county = models.CharField(max_length=80,
-                              null=True,
                               blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6,
@@ -106,7 +111,6 @@ class OrderLineItem(models.Model):
                                 on_delete=models.CASCADE,
                                 related_name='lineitems')
     product_size = models.CharField(max_length=2,
-                                    null=True,
                                     blank=True)
     quantity = models.IntegerField(null=False,
                                    blank=False,
