82dfddde5872b41df914f1fb0e1168ec306e99fe
PeterKellett <peterwkellett@gmail.com>
2020-12-08 12:47:24 +0000

    Redeploy commit after reopening workspace from hibernation

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,6 +10,7 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+    print(bag)
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
